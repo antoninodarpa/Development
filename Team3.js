5336c2224fe4d4172b04bfb959e2ab985b385597
Vittorio De Giorgio <vdegiorgio8@gmail.com>
2023-06-10 18:28:07 +0200

    Ho inserito il mio oggetto 'students' nel mio branch 'Vittorio'.

--- Team3.js
+++ Team3.js
@@ -1,1 +1,13 @@
-let team3 =[];
+let team3Students =[{
+    name: "Vittorio",
+  surname: "De Giorgio",
+  age: 26,
+  city: "Amaroni",
+  hobby: "Gym",
+  favouriteFood: "Pizza",
+  favouriteVideoGame: "The last of us",
+  favouriteFilm: "Interstellar",
+  favouriteBook: "Io, Ibra",
+  petName: "McGiò"
+};
+];
