41e95c6585093f4b11fd5653dade21e917b4d7e9
Antonino <antoninodarpait@gmail.com>
2023-06-13 16:04:20 +0200

    esercizio antonino

--- Team3.js
+++ Team3.js
@@ -1,1 +1,15 @@
 let team3 =[];
+
+
+const Antonino = {
+    name: "Antonino",
+    surname: "D'Arpa",
+    age: 28,
+    city: "Palermo",
+    hobby: "Digital Collage",
+    favoriteFood: "Lasagne",
+    favoriteVideoGame: "7 Wonders",
+    favoriteFilm: "American Beauty",
+    favoriteBook: "Da Cosa Nasce Cosa",
+    petName: "Bobby",
+  };
