9383a68f37506b6d71d6652edd05f8439d1f9f8c
Vittorio <vdegiorgio8@gmail.com>
2023-06-13 21:40:00 +0200

    Ho modificato il file Team3.js nel branch "main" perchè era stato caricato un oggetto in quel branch direttamente.

--- Team3.js
+++ Team3.js
@@ -1,15 +1,1 @@
 let team3 = [];
-
-const leo = [{
-  name: "leo",
-  surname: "sorre",
-  age: 27,
-  city: "bologna",
-  hobby: "GO",
-  favoriteFood: "amatriciana",
-  favoriteVideoGame: "fallout",
-  favoriteFilm: "dune",
-  favoriteBook: "dune",
-  petName: "Katana",
-};
-];
