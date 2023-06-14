8291128055543e77081c28928524d0231457b4e6
stefu10 <127099776+stefu10@users.noreply.github.com>
2023-06-14 12:55:36 +0200

    stefano

--- Team3.js
+++ Team3.js
@@ -1,26 +1,14 @@
-let team3Students =[{
-    name: "Vittorio",
-  surname: "De Giorgio",
-  age: 26,
-  city: "Amaroni",
-  hobby: "Gym",
-  favouriteFood: "Pizza",
-  favouriteVideoGame: "The last of us",
-  favouriteFilm: "Interstellar",
-  favouriteBook: "Io, Ibra",
-  petName: "McGiò"
-}
-{
-  name: "Antonino",
-  surname: "D'Arpa",
-  age: 28,
-  city: "Palermo",
-  hobby: "Digital Collage",
-  favoriteFood: "Lasagne",
-  favoriteVideoGame: "7 Wonders",
-  favoriteFilm: "American Beauty",
-  favoriteBook: "Da Cosa Nasce Cosa",
-  petName: "Bobby",
+const Stefano
+= {
+  name: "Stefano",
+  surname: "Mazzarella",
+  age: 21,
+  city: "Napoli",
+  hobby: "football",
+  favoriteFood: "pasta al ragù",
+  favoriteVideoGame: "fifa",
+  favoriteFilm: "peaky blinders",
+  favoriteBook: "la vera storia dei peaky blinders",
+  petName: "Roy",
 };
-];
 
